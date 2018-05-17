d0f3f05eef6d8cafaec85678e224e3cb73682df1
Jeff <jeff@JeffdeMBP.Home>
2018-05-18 00:06:59 +0800

    The first page

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, '/public/')));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/', '/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server start: listening on port ${chalk.green(port)}`);
+  debug(`listening on port ${chalk.green(port)}`);
+});
---
+++ readme.txt
@@ -0,0 +1,7 @@
+run the web site
+    node app.js
+
+run the web site in debug mode
+    DEBUG=* node app.js
+    or
+    DEBUG=app node app.js
