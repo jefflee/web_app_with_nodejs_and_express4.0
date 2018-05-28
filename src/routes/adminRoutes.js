80d4fe194a2ce1f63ddd5b7c68436434cc5d057d
Jeff <jeff@JeffdeMBP.Home>
2018-05-28 22:38:26 +0800

    Invoke GoodReads API to display book detail

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -10,47 +10,55 @@ const books = [
     title: 'War and Peace',
     genre: 'Historical Fiction',
     author: 'Lev Nikolayevich Tolstoy',
+    bookId: 656,
     read: false
   },
   {
     title: 'Les Misérables',
     genre: 'Historical Fiction',
     author: 'Victor Hugo',
+    bookId: 24280,
     read: false
   },
   {
     title: 'The Time Machine',
     genre: 'Science Fiction',
     author: 'H. G. Wells',
+    bookId: 1000,
     read: false
   },
   {
     title: 'A Journey into the Center of the Earth',
     genre: 'Science Fiction',
     author: 'Jules Verne',
+    bookId: 2000,
     read: false
   },
   {
     title: 'The Dark World',
     genre: 'Fantasy',
     author: 'Henry Kuttner',
+    bookId: 321,
     read: false
   },
   {
     title: 'The Wind in the Willows',
     genre: 'Fantasy',
     author: 'Kenneth Grahame',
+    bookId: 900,
     read: false
   },
   {
     title: 'Life On The Mississippi',
     genre: 'History',
     author: 'Mark Twain',
+    bookId: 1234,
     read: false
   },
   {
     title: 'Childhood',
     genre: 'Biography',
+    bookId: 333,
     author: 'Lev Nikolayevich Tolstoy',
     read: false
   }];
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,87 +1,17 @@
 const express = require('express');
-const { MongoClient, ObjectID } = require('mongodb');
-const debug = require('debug')('app:bookRoutes');
-const mongoDbUrl = require('../../mongoDbUrl');
+const bookController = require('../controllers/bookController');
+const bookService = require('../services/goodreadsService');
 
 const bookRouter = express.Router();
 
 function router(nav) {
-  bookRouter.use((req, res, next) => {
-    if (req.user) {
-      next();
-    } else {
-      res.redirect('/');
-    }
-  });
+  const { getIndex, getById, middleware } = bookController(bookService, nav);
+  bookRouter.use(middleware);
   bookRouter.route('/')
-    .get((req, res) => {
-      const url = mongoDbUrl;
-      const dbName = 'libraryApp';
-      debug('Before invoke IIFE function.');
-      (async function mongo() {
-        let client;
-        try {
-          debug('Before Connected correctly to server');
-          client = await MongoClient.connect(url);
-          debug('After connected correctly to server');
-
-          const db = client.db(dbName);
-
-          debug('Before open collection books');
-          const col = await db.collection('books');
-          debug('After open collection  books');
-
-          debug('Before get all books');
-          const books = await col.find().toArray();
-          debug('After get all books');
-
-          res.render(
-            'bookListView',
-            {
-              nav,
-              title: 'Library',
-              books
-            }
-          );
-        } catch (err) {
-          debug(err.stack);
-        }
-        client.close();
-      }());
-      debug('After invoke IIFE function.');
-    });
+    .get(getIndex);
 
   bookRouter.route('/:id')
-    .get((req, res) => {
-      const { id } = req.params;
-      const url = mongoDbUrl;
-      const dbName = 'libraryApp';
-
-      (async function mongo() {
-        let client;
-        try {
-          client = await MongoClient.connect(url);
-          debug('Connected correctly to server');
-
-          const db = client.db(dbName);
-
-          const col = await db.collection('books');
-
-          const book = await col.findOne({ _id: new ObjectID(id) });
-          debug(book);
-          res.render(
-            'bookView',
-            {
-              nav,
-              title: 'Library',
-              book
-            }
-          );
-        } catch (err) {
-          debug(err.stack);
-        }
-      }());
-    });
+    .get(getById);
   return bookRouter;
 }
 
